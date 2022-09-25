b0530cd48859959634b3afad192497881183542c
Larissa <larissa.fraga@ufms.br>
2022-09-25 19:07:23 -0400

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title class="toolbar-title">
+          Dollar Conversion
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+})
+</script>
+
+<style lang="scss" scoped>
+.toolbar-title {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,96 @@
+<template>
+  <q-page class="flex items-center justify-center">
+    <div class="container">
+      <q-input
+        class="input"
+        outlined
+        v-model="real"
+        prefix="R$"
+        input-class="text-right"
+        reverse-fill-mask
+        style="margin-bottom: 1rem"
+      />
+      <q-input
+        class="input"
+        outlined
+        v-model="dollar"
+        prefix="US$"
+        input-class="text-right"
+        reverse-fill-mask
+      />
+      <div class="button-container">
+        <q-btn
+          color="white"
+          text-color="primary"
+          label="Clear"
+          @click="clearFields()"
+        />
+        <q-btn
+          color="primary"
+          label="Calculate"
+          @click="calculateConversion()"
+        />
+      </div>
+      <p class="text" v-if="showTotal">
+        With R${{ real }} it is possible to buy US${{ dollar }} at
+        {{ total }} dollars each.
+      </p>
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage',
+
+  data() {
+    return {
+      real: 0,
+      dollar: 0,
+      total: 0,
+      showTotal: false,
+    }
+  },
+  methods: {
+    clearFields() {
+      this.real = 0
+      this.dollar = 0
+      this.showTotal = false
+    },
+    calculateConversion() {
+      this.total = this.real / this.dollar
+      this.total = parseFloat(this.total.toFixed(2))
+      this.showTotal = true
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.input {
+  margin-bottom: 1rem;
+  width: 215px;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.button-container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 215px;
+}
+
+.text {
+  margin-top: 1rem;
+  text-align: center;
+}
+</style>
